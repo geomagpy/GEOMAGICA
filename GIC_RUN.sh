9aa05bd6b44c9a98a84f27a60a1d6be2128a0bec
R Bailey <rachel.bailey@zamg.ac.at>
2018-05-16 14:12:09 +0200

    Updated GIC_RUN.sh with new model option for completeness.

--- GIC_RUN.sh
+++ GIC_RUN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DBFILE="dB_2017-09-07T23:25:00.txt"
+DATESTR="2017-09-07T23:25:00"
 MODEL1D=39
 MODEL2D="aerosq1000"
 P=300
@@ -8,7 +8,9 @@ P=300
 gfortran -std=legacy -g thinsheet/nonuni_short_2d.f thinsheet/anomal.f thinsheet/seidel_new.f90 thinsheet/newmodel.f -o thinsheet.exe
 
 # Run FORTRAN code:
+DBFILE="dB_${DATESTR}.txt"
 ./thinsheet.exe $DBFILE $P $MODEL1D ts_$MODEL2D.txt
 
 # Compute GIC with output E-field file:
-#python GIC_Model_Horton.py -d $DATESTR -p $P -l $MODEL1D -s $MODEL2D
+EFILE="Efiles/E_${MODEL1D}_${DATESTR}.txt"
+python GIC_Model_Horton.py -e $EFILE
